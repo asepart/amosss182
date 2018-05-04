63767ee0a20a2b66ddeacefa803ad2648828516e
David Haller <davidhaller@mailbox.org>
2018-05-04 15:48:42 +0200

    add OPTIONS request

--- webservice/src/main/java/de/fau/cs/osr/amos/asepart/WebService.java
+++ webservice/src/main/java/de/fau/cs/osr/amos/asepart/WebService.java
@@ -7,11 +7,7 @@ import java.util.List;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -30,6 +26,13 @@ import de.fau.cs.osr.amos.asepart.entities.*;
 @Path("/")
 public class WebService
 {
+    @OPTIONS
+    @PermitAll
+    public Response options()
+    {
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+
     @Path("/login")
     @GET
     @RolesAllowed({"Admin", "User"})
