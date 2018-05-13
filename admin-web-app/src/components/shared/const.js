ac836daafa0cf743807e037d607fa81a52bbdbb5
dudas <git@sebdu.de>
2018-05-13 15:03:25 +0200

    Added mechanism to determine the URL of the backend depending on the kind of deployment

--- admin-web-app/src/components/shared/const.js
+++ admin-web-app/src/components/shared/const.js
@@ -1,1 +1,10 @@
-export const URL = "http://localhost:12345";
+export const URL = getURL();
+
+function getURL(){
+	if (window.location.hostname.indexOf("dev.herokuapp") !== -1){
+		return "https://asepartback-dev.herokuapp.com/";
+	} else if (window.location.hostname.indexOf("localhost") !== -1){
+		return "http://localhost:12345";
+	}
+	return "https://asepartweb.herokuapp.com/";
+}
