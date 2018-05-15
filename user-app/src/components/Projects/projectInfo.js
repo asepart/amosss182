428d69be6f8a4e44eab1bd5b19a7e559fab8c988
Long Do <33006592+longpdo@users.noreply.github.com>
2018-05-15 17:58:15 +0200

    Added show ticket code
    need to fix: projectname is still hardcoded

--- user-app/src/components/Projects/projectInfo.js
+++ user-app/src/components/Projects/projectInfo.js
@@ -1,12 +1,38 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ActivityIndicator, FlatList} from 'react-native';
 import styles from '../Login/Design';
 import {
 	StackNavigator,
   } from 'react-navigation';
+import {getAuth} from '../Login/auth';
+import {URL} from '../Login/const';
+import {key} from './keyValid';
 
 export default class ProjectInfo extends Component {
 
+  constructor(props) {
+		super(props);
+		this.state = {
+			isLoading: true
+		}
+	}
+  
+  componentDidMount() {
+		var url = URL;
+        var name = 'testproject';
+		url += '/projects/' + name + '/tickets';
+		return fetch(url, {method:'GET', headers: getAuth()})
+		.then((response) => response.json())
+		.then((responseJson) => {
+			this.setState({
+				isLoading: false,
+				dataSource: responseJson
+			}, function() {});
+		}).catch((error) => {
+			console.error(error);
+		});
+	}
+  
   static navigationOptions= {
 		title: 'Project Info',
 		headerStyle: {
@@ -20,14 +46,23 @@ export default class ProjectInfo extends Component {
 
     render() {
       var {params} = this.props.navigation.state;
+      
+      if (this.state.isLoading) {
+			return (
+				<View style={{flex: 1,padding: 20}}>
+					<ActivityIndicator/>
+				</View>
+			)
+		}
+     
       return (
         <View style={styles.container}>
-           
-			
-				<Text style={styles.buttonText && styles.textLarge}>Here will be project info if key was valid</Text>
-
-		
-        
+                <FlatList
+                  style={styles.textLarge}
+                  data={this.state.dataSource}
+                  renderItem={({item}) => <Text>{item.id}, {item.ticketSummary}, {item.ticketCategory}</Text>}
+                  keyExtractor={(item, index) => index}
+                />
         </View>
       );
     }
