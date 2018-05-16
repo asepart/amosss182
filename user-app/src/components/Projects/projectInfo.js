b8ec2a63d3953c0afb91c6b4850f7e5dedf77af7
Long Do <33006592+longpdo@users.noreply.github.com>
2018-05-16 08:40:08 +0200

    solved getProjectName without hardcoded part

--- user-app/src/components/Projects/projectInfo.js
+++ user-app/src/components/Projects/projectInfo.js
@@ -4,36 +4,58 @@ import styles from '../Login/Design';
 import {
 	StackNavigator,
   } from 'react-navigation';
-import {getAuth} from '../Login/auth';
+import {getAuth,username,psw} from '../Login/auth';
 import {URL} from '../Login/const';
-import {key} from './keyValid';
+import {key, getProjectName} from './keyValid';
 
 export default class ProjectInfo extends Component {
 
   constructor(props) {
 		super(props);
 		this.state = {
-			isLoading: true
-		}
+			isLoading: true,
+            projectName: '',
+            ticketList: [],
+		};
 	}
   
   componentDidMount() {
-		var url = URL;
-        var name = 'testproject';
-		url += '/projects/' + name + '/tickets';
-		return fetch(url, {method:'GET', headers: getAuth()})
-		.then((response) => response.json())
-		.then((responseJson) => {
-			this.setState({
+    
+        fetch('https://asepartback-dev.herokuapp.com/join?key=' + key, 
+              {method:'GET', headers: 
+                 {'X-ASEPART-Role': 'User',
+                  'Authorization': 'Basic ' + btoa(username + ":" + psw)
+                 }
+              }
+        )
+        .then(response => {
+          return response.text();
+        }).then(responseText => {
+          if(responseText !== '') {
+            this.setState({
 				isLoading: false,
-				dataSource: responseJson
+				projectName: responseText,
 			}, function() {});
+            
+            var url = URL;
+            url += '/projects/' + this.state.projectName + '/tickets';
+            return fetch(url, {method:'GET', headers: getAuth()})
+              .then((response) => response.json())
+              .then((responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    ticketList: responseJson
+                }, function() {});
+              }).catch((error) => {
+                console.error(error);
+              }); 
+          }
 		}).catch((error) => {
 			console.error(error);
 		});
 	}
   
-  static navigationOptions= {
+    static navigationOptions= {
 		title: 'Project Info',
 		headerStyle: {
 			backgroundColor:'#8eacbb'
@@ -43,10 +65,8 @@ export default class ProjectInfo extends Component {
 		}
     } 
 
-
     render() {
       var {params} = this.props.navigation.state;
-      
       if (this.state.isLoading) {
 			return (
 				<View style={{flex: 1,padding: 20}}>
@@ -59,10 +79,10 @@ export default class ProjectInfo extends Component {
         <View style={styles.container}>
                 <FlatList
                   style={styles.textLarge}
-                  data={this.state.dataSource}
+                  data={this.state.ticketList}
                   renderItem={({item}) => <Text>{item.id}, {item.ticketSummary}, {item.ticketCategory}</Text>}
                   keyExtractor={(item, index) => index}
-                />
+                ></FlatList>
         </View>
       );
     }
