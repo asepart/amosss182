dc7c66136af2669ea29f816102e53ae70b007ec9
Long Do <33006592+longpdo@users.noreply.github.com>
2018-05-23 23:59:45 +0200

    Create TicketChatButton.js

---
+++ admin-web-app/src/components/Chat/TicketChatButton.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { setState } from '../shared/GlobalState';
+
+export default class TicketChatButton extends Component {
+
+  showTicketChat() {
+		setState({
+			isAuth: true,
+			show: 'ticketChat',
+			param: this.props.keyFromParent,
+			name: this.props.nameFromParent,
+      id: this.props.proj.row.id
+		});
+	}
+
+	render() {
+		return (	// TODO: add edit icon instead of text here? maybe bubble
+			<Text
+				onPress = { this.showTicketChat.bind(this) }
+				style={{color: '#5daedb'}}
+			>
+				CHAT
+			</Text>
+		);
+	}
+}
