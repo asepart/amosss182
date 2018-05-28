dc4d3a478781d132a354c1b29f4652a6bc6a1646
David Haller <davidhaller@mailbox.org>
2018-05-28 18:34:23 +0200

    bugfix: wrong URL returned if hostname of developer machine is not localhost

--- admin-web-app/src/components/shared/const.js
+++ admin-web-app/src/components/shared/const.js
@@ -4,10 +4,13 @@ export const cki = new Cookies(document.cookie);
 export const URL = getURL();
 
 function getURL(){
-	if (window.location.hostname.indexOf("dev.herokuapp") !== -1){
+	if (window.location.hostname.indexOf("asepartweb-dev.herokuapp") !== -1){
 		return "https://asepartback-dev.herokuapp.com";
-	} else if (window.location.hostname.indexOf("localhost") !== -1){
-		return "http://localhost:12345";
+	} 
+	
+	else if (window.location.hostname.indexOf("asepartweb.herokuapp") !== -1){
+		return "https://asepartback.herokuapp.com";
 	}
-	return "https://asepartweb.herokuapp.com";
+	
+	else return "http://localhost:12345";
 }
