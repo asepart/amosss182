c95d6ef2b4fe4d44f6f1661d114e82aa6afb9904
tanjabatz <tanja.batz5@gmail.com>
2018-06-03 11:41:38 +0200

    added picker for choosing outcome and input field for numbers of observations

--- user-app/src/components/Tickets/ticketProcessed.js
+++ user-app/src/components/Tickets/ticketProcessed.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Picker} from 'react-native';
 import styles from '../Login/Design';
 import {
 	StackNavigator,
@@ -7,19 +7,37 @@ import {
 import {URL} from '../Login/const';
 import {key} from './keyValid';
 
+var pickerPlaceholder = "Outcome";
+
 export default class TicketProcessing extends Component {
 
     constructor() {
 		super();
 
 		this.state = {
-			
+            ticketOutcome: "",
+            observations: ""
 		}
     }
     
     render() {
         return (
             <View style={styles.container}>
+					<Picker
+						style = {{height: 40, backgroundColor: 'transparent', borderColor: 'gray', borderWidth: 1, textAlign: 'center'}}
+						onValueChange = {(text) => this.setState({ticketOutcome: text})}
+						selectedValue = {this.state.ticketOutcome}
+					>
+						<Picker.Item label = {pickerPlaceholder} value = {pickerPlaceholder} />
+						<Picker.Item label = "Positive" value = "Positive" />
+						<Picker.Item label = "Negative" value = "Negative" />
+					</Picker>
+					<TextInput
+						placeholder = "Observations"
+						style = {styles.input}
+						onChangeText = {(text) => this.setState({observations: text})}
+						value = {this.state.observations}
+					/>
             </View>
         );
 
