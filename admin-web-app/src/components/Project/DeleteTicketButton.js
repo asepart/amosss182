a123348789b5505fb3d9ff4353256e4d73f532b2
Long Do <33006592+longpdo@users.noreply.github.com>
2018-06-04 16:49:04 +0200

    added popup code for deleting tickets [skip ci]

--- admin-web-app/src/components/Project/DeleteTicketButton.js
+++ admin-web-app/src/components/Project/DeleteTicketButton.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import { setState } from '../shared/GlobalState';
+import Popup from "reactjs-popup";
+import {getAuth} from '../shared/auth';
+import {URL} from '../shared/const';
 
 export default class DeleteTicketButton extends Component {
 
-  showDeleteTicketConfirm() {
-		setState({
-			isAuth: true,
-			show: 'deleteTicket',
-			param: this.props.keyFromParent,
-			name: this.props.nameFromParent,
-			id: this.props.proj.row.id,
-			tName: this.props.proj.row.ticketName
-		});
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+  openPopup = () => {
+    this.setState({ open: true });
+  };
+  closePopup = () => {
+    this.setState({ open: false });
+  };
+
+  deleteTicket() {
+    var url = URL;
+		url += '/projects/' + this.props.keyFromParent + '/tickets/' + this.props.proj.row.id;
+    fetch(url, {method:'DELETE', headers: getAuth()})
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error(error);
+      });
+    this.setState({
+        open: false
+  	})
 	}
 
 	render() {
 		return (	// TODO: add edit icon instead of text here
-			<Text
-				onPress = { this.showDeleteTicketConfirm.bind(this) }
-				style={{color: '#5daedb'}}
-			>
-				DELETE
-			</Text>
+      <div>
+        <button onClick={this.openPopup} style={{color: '#5daedb'}}>
+          DELETE
+        </button>
+        <Popup
+          open={this.state.open}
+          closeOnDocumentClick
+          onClose={this.closePopup}
+        >
+          <View>
+          <Button
+            onPress = { function doNothing() {} }
+            disabled = {true}
+            title = {"Delete " + this.props.proj.row.ticketName + " from " + this.props.nameFromParent + "?"}
+          />
+          <Button onPress = { this.deleteTicket.bind(this) } title = "Delete" color = "#0c3868"/>
+          <Button onPress = { this.closePopup } title = "Cancel" color = "#0e4a80" />
+          </View>
+        </Popup>
+      </div>
 		);
 	}
 }
