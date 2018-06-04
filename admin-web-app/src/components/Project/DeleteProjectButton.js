5946fd27ab68a014ae34608bc1140225b2c33364
Long Do <33006592+longpdo@users.noreply.github.com>
2018-06-04 16:13:15 +0200

    added popup code for deleting projects [skip ci]

--- admin-web-app/src/components/Project/DeleteProjectButton.js
+++ admin-web-app/src/components/Project/DeleteProjectButton.js
@@ -1,27 +1,58 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-import { setState } from '../shared/GlobalState';
-import { Link } from 'react-router-dom';
+import { View, Button } from 'react-native';
+import Popup from "reactjs-popup";
+import {getAuth} from '../shared/auth';
+import {URL} from '../shared/const';
 
 export default class DeleteProjectButton extends Component {
 
-  showDeleteProjectConfirm() {
-		setState({
-			isAuth: true,
-			show: 'deleteProject',
-			param: this.props.proj.row.entryKey,
-			name: this.props.proj.row.projectName
-		});
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+  openPopup = () => {
+    this.setState({ open: true });
+  };
+  closePopup = () => {
+    this.setState({ open: false });
+  };
+
+  deleteProject() {
+    var url = URL;
+		url += '/projects/' + this.props.proj.row.entryKey;
+    fetch(url, {method:'DELETE', headers: getAuth()})
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error(error);
+      });
+    this.setState({
+      open: false,
+		})
+    this.forceUpdate()
 	}
 
 	render() {
 		return (	// TODO: add edit icon instead of text here
-				<Text
-					onPress = { this.showDeleteProjectConfirm.bind(this) }
-					style={{color: '#5daedb'}}
-				>
-					DELETE
-				</Text>
+        <div>
+          <button onClick={this.openPopup} style={{color: '#5daedb'}}>
+            DELETE
+          </button>
+          <Popup
+            open={this.state.open}
+            closeOnDocumentClick
+            onClose={this.closeModal}
+          >
+            <View>
+            <Button
+              onPress = { function doNothing() {} }
+              disabled = {true}
+              title = {"Delete " + this.props.proj.row.projectName + "?"}
+            />
+            <Button onPress = { this.deleteProject.bind(this) } title = "Delete" color = "#0c3868"/>
+            <Button onPress = { this.closePopup } title = "Cancel" color = "#0e4a80" />
+            </View>
+          </Popup>
+        </div>
 		);
 	}
 }
