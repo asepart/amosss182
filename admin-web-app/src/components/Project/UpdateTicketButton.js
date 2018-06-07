e3ab4a233d6a61f1c56d2ce0ef37b6af36de498c
Long Do <33006592+longpdo@users.noreply.github.com>
2018-06-07 15:52:03 +0200

    added refresh page after updating a ticket [skip ci]

--- admin-web-app/src/components/Project/UpdateTicketButton.js
+++ admin-web-app/src/components/Project/UpdateTicketButton.js
@@ -4,6 +4,7 @@ import Popup from "reactjs-popup";
 import {getAuthForPost} from '../shared/auth';
 import {URL} from '../shared/const';
 import '../../index.css';
+import {setUpdateBoolean} from '../shared/GlobalState';
 
 var pickerPlaceholder = "Category";
 
@@ -50,18 +51,14 @@ export default class UpdateTicketButton extends Component {
 			})
 			.then((response) => response.json())
 			.then((responseJson) => {
-				this.setState({
-					ticketName: "",
-					ticketSummary: "",
-					ticketDescription: "",
-					ticketCategory: "",
-					requiredObservations: "",
-					id: ""
-				}, function() {});
+				this.setState({}, function() {});
 			})
 			.catch((error) => {
 				console.error(error);
 			});
+
+		this.props.callToParent();
+		setUpdateBoolean(true);
 		this.setState({
 		  open: false
 		})
