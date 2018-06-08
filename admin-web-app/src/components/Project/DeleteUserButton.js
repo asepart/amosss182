f5d5a823c95005f00131104ba4ae407061690ef4
Long Do <33006592+longpdo@users.noreply.github.com>
2018-06-08 13:37:26 +0200

    replaced button with delete icon [skip ci]

--- admin-web-app/src/components/Project/DeleteUserButton.js
+++ admin-web-app/src/components/Project/DeleteUserButton.js
@@ -45,11 +45,9 @@ export default class DeleteUserButton extends Component {
       projectName = " from " + this.props.nameFromParent;
     }
 
-		return (	// TODO: add edit icon instead of text here
+		return (
       <div>
-        <button onClick={this.openPopup} style={{color: '#5daedb'}}>
-          DELETE
-        </button>
+        <img onClick={this.openPopup} style={{height: 25, marginBottom: -5}} src={require('../images/delete.png')} alt=""/>
         <Popup
           open={this.state.open}
           closeOnDocumentClick
