6982855d6764510da44fd31141b442d615535fdc
David Haller <davidhaller@mailbox.org>
2018-06-08 18:05:45 +0200

    added n:m relationships

--- webservice/data/tables.sql
+++ webservice/data/tables.sql
@@ -17,7 +17,7 @@ create table users (
 create table projects (
   entry_key character varying (32) primary key,
   name character varying (32) not null,
-  owner character varying (32) not null references admins(login_name)
+  owner character varying (32) not null references admins(login_name) on delete restrict
 );
 
 create type ticket_category as enum ('one-time-error', 'trace', 'behavior');
@@ -31,12 +31,34 @@ create table tickets(
   category ticket_category not null,
   status ticket_status not null,
   required_obversations integer not null check (required_obversations >= 0),
-  project_key character varying (32) not null references projects(entry_key)
+  project_key character varying (32) not null references projects(entry_key) on delete cascade
 );
 
 create table message(
   id serial primary key,
   sender character varying (32) not null references accounts(login_name),
   content text not null,
-  ticket_id serial not null references tickets(id)
+  ticket_id serial not null references tickets(id) on delete cascade
 );
+
+create table memberships(
+  project_key character varying (32) not null references projects(entry_key) on delete cascade,
+  login_name character varying (32) not null references users(login_name) on delete cascade,
+  primary key (project_key, login_name)
+);
+
+create table assignments(
+  ticket_id serial not null references tickets(id) on delete cascade,
+  login_name character varying (32) not null references users(login_name) on delete cascade,
+  primary key (ticket_id, login_name)
+);
+
+create type observation_outcome as enum ('positive', 'negative');
+
+create table observations(
+  ticket_id serial not null references tickets(id) on delete cascade,
+  login_name character varying (32) not null references users(login_name) on delete cascade,
+  outcome observation_outcome not null,
+  quantity integer not null check (quantity > 0),
+  primary key (ticket_id, login_name)
+)
