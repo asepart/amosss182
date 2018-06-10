beee6052da01697fb3ca354ae531a5d6f945b0b0
8sd <git@sebdu.de>
2018-06-10 12:33:02 +0200

    Ticket list shows real state

--- admin-web-app/src/components/Project/TicketStatus.js
+++ admin-web-app/src/components/Project/TicketStatus.js
@@ -3,7 +3,7 @@ import { View, Button, TextInput } from 'react-native';
 
 export default class TicketStatus extends Component {
 	render() {
-		if(this.props.state === 'FINISHED'){
+		if(this.props.state.value === 'FINISHED'){
 			return (
 				<button  style={{color: '#5daedb'}}>
 					FINISHED
