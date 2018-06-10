98d4e74aaf24af30237910740f0262443f791c56
David Haller <davidhaller@mailbox.org>
2018-06-10 16:11:05 +0200

    try to fix permission error

--- webservice/data/setup.sh
+++ webservice/data/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 psql -c "create database asepartdb;" -U postgres
-psql -c "create user asepart with encrypted password 'asepart';" -U postgres
-psql -c "grant all privileges on database asepartdb to asepart;" -U postgres
+
 psql -d asepartdb -c "create extension pgcrypto;" -U postgres
 psql -d asepartdb -f schema.sql -U postgres
+
+psql -c "create user asepart with encrypted password 'asepart';" -U postgres
+psql -c "grant all privileges on database asepartdb to asepart;" -U postgres
 
 exit 0
