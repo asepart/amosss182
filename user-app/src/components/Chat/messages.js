43652e8423585f21fd57563882a7c930f6ffb2da
Long Do <33006592+longpdo@users.noreply.github.com>
2018-06-13 00:26:18 +0200

    integrated giftedchat library into our chat
    - mapped the message array from our API into the giftedchat array.
    - maintained all functionality until now (e.g. sending message, seeing chat time)
    - added an options button for uploading different media

--- user-app/src/components/Chat/messages.js
+++ user-app/src/components/Chat/messages.js
@@ -1,20 +1,17 @@
 import React, {Component} from 'react';
-import { FlatList, ActivityIndicator, Text, View, TextInput, TouchableOpacity, ScrollView, Button } from 'react-native';
+import { Platform, ActivityIndicator, View } from 'react-native';
 import {URL} from '../Login/const';
-import {getAuth} from '../Login/auth';
+import {getAuth, username} from '../Login/auth';
 import styles from '../Login/Design';
 import {setState} from '../Login/state';
 import {setMsg, sendMessage, setTicketID} from './sendMessages'
-import {ProjectInfo} from '../Projects/projectInfo';
-import {TicketView} from '../Projects/ticketView';
 import {ticket} from './sendMessages';
-import {
-	StackNavigator,
-  } from 'react-navigation';
+import {StackNavigator} from 'react-navigation';
+import { GiftedChat, Actions } from 'react-native-gifted-chat'
 
 export default class Messages extends Component {
 
-  static navigationOptions= {
+	static navigationOptions= {
 		title: 'Chat',
 		headerStyle: {
 			backgroundColor:'#5daedb'
@@ -24,35 +21,35 @@ export default class Messages extends Component {
 		}
 	}
 
-  constructor(props){
-    super(props);
-    this.state = {
+	constructor(props){
+		super(props);
+		this.state = {
 			isLoading: true,
 			message: "",
 			error: "",
-    	//idTicket: ""
-    }
-  }
-
-	componentDidMount(){
-    this.makeApiCall();
-  }
-
-	async makeApiCall() {
-	  return fetch(URL + '/messages/' + ticket , {method:'GET', headers: getAuth()})
-	  .then((response) => response.json())
-	  .then((responseJson) => {
-	    this.setState({
-	      isLoading: false,
-	      dataSource: responseJson,
-	    }, function(){});
-	  })
-	  .catch((error) =>{
-	    console.error(error);
-	  });
+			//idTicket: ""
+		}
 	}
 
-  async onSendPressed() {
+	componentDidMount(){
+		this.makeApiCall();
+	}
+
+	async makeApiCall() {
+		return fetch(URL + '/messages/' + ticket , {method:'GET', headers: getAuth()})
+		.then((response) => response.json())
+		.then((responseJson) => {
+			this.setState({
+				isLoading: false,
+				dataSource: responseJson,
+			}, function(){});
+		})
+		.catch((error) =>{
+			console.error(error);
+		});
+	}
+
+	async onSendPressed() {
 		var tmp = new Date();
 		//+1 is needed, since getMonth returns 0-11
 		var date = tmp.toDateString();
@@ -60,78 +57,65 @@ export default class Messages extends Component {
 		var timestamp = "[" + date + ", " + time + "]";
 
 		setMsg(timestamp + ": " + this.state.message);
-    sendMessage();
-    this.makeApiCall();
+		sendMessage();
+		this.makeApiCall();
 	}
 
-	renderChat() {
-    var tmp_chat = this.state.dataSource;
-    var tmp_date;
+	//TODO: add actions like upload photo, video, document
+	renderActions(props) {
+		const options = {
+			'Upload 1': (props) => {
+				alert('option 1');
+			},
+			'Upload 2': (props) => {
+				alert('option 2');
+			},
+			'Cancel': () => {},
+		};
+		return (
+			<Actions
+				{...props}
+				options={options}
+			/>
+		);
+	}
 
-    return this.state.dataSource.map(function(news, id) {
-      if(id !== 0) {
-        tmp_date = tmp_chat[id-1].content.slice(1,16);
-      } else {
-        tmp_date = new Date(1993, 3, 20);
-      }
-      return (
-        <View key={id}>
-          <View>
-            {tmp_date !== news.content.slice(1,16) ? (
-                <Button
-                  disabled = {true}
-                  title = {news.content.slice(1,16)}
-                />
-            ) : (
-              null
-            )}
-          </View>
-          <Text style={{fontWeight: 'bold'}}>
-						[{news.content.slice(18,27)} {news.sender}: {news.content.slice(29)}
-					</Text>
-        </View>
-      );
-    });
-  }
-
-  render() {
-    if(this.state.isLoading) {
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
-        </View>
-      )
-    }
-
-    return(
-      <View style={styles.containerChat}>
-				<ScrollView
-					ref = {ref => this.scrollView = ref}
-				  onContentSizeChange = {(contentWidth, contentHeight) => {
-						this.scrollView.scrollToEnd({animated: false});
-					}}
-				>
-        	{this.renderChat()}
-				</ScrollView>
-
-				<View style={{height: 5}}/>
-
-				<View style={{flexDirection: 'row'}}>
-	        <TextInput
-						onChangeText={(text) => this.setState({message: text})}
-						placeholder="Message"
-						underlineColorAndroid="transparent"
-						style={styles.sendTextInput}
-					/>
-
-	        <TouchableOpacity
-	           onPress={this.onSendPressed.bind(this)}
-	            style={styles.sendButton}
-					>
-						<Text style={styles.buttonText}>SEND</Text>
-					</TouchableOpacity>
+	render() {
+		if(this.state.isLoading) {
+			return(
+				<View style={{flex: 1, padding: 20}}>
+					<ActivityIndicator/>
 				</View>
-      </View>
-    );
-  }
+			)
+		}
+
+		//remap dataSource to GiftedChat supported object array
+		const messages = this.state.dataSource.map((message) => {
+			return {
+				_id: message.id,
+				text: message.content.slice(29),
+				user: Object.assign({_id: message.sender, name: message.sender}),
+				createdAt: message.content.slice(1,26),
+			};
+		});
+
+		//GiftedChat somehow shows the newest message at the top right now
+		//inverted={false} does solve this but also breaks the position of the date
+		//better fix right now: reversing the messages array before rendering via GiftedChat
+		messages.reverse();
+
+		return(
+			<GiftedChat
+				messages={messages}
+				onInputTextChanged={(text) => this.setState({message: text})}
+				onSend={this.onSendPressed.bind(this)}
+				showAvatarForEveryMessage={true}
+				renderActions={this.renderActions}
+				user={{
+					_id: username,
+					name: username,
+				}}
+			/>
+		);
+	}
 }
