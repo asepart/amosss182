c0b4abe6da2147fd4c3ec0a46f6b1029935ecf88
tanjabatz <tanja.batz5@gmail.com>
2018-06-23 18:13:11 +0200

    added react native camera library`

--- user-app/src/components/Login/const.js
+++ user-app/src/components/Login/const.js
@@ -1,3 +1,3 @@
-export const URL = "https://asepartback-dev.herokuapp.com";
-//const myIp = ""; //type local ip adress
-//export const URL = "http://" + myIp + ":12345"; //enable this line for local testing 
+//export const URL = "https://asepartback-dev.herokuapp.com";
+const myIp = "192.168.2.119"; //type local ip adress
+export const URL = "http://" + myIp + ":12345"; //enable this line for local testing 
