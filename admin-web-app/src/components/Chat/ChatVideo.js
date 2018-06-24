a51e629d3b695014124f40801915faf6a1090ef2
8sd <git@sebdu.de>
2018-06-24 11:04:25 +0200

    fix copy past error

--- admin-web-app/src/components/Chat/ChatVideo.js
+++ admin-web-app/src/components/Chat/ChatVideo.js
@@ -10,7 +10,7 @@ export default class ChatVideo extends Component {
 			<Image
 				source={require(this.props.children.preview)}
 				style={{width: 100, height: 100}}
-				onPress={this.viewVideo(this.props.children.image)}
+				onPress={this.viewVideo(this.props.children.video)}
 			/>
 		);
 	}
