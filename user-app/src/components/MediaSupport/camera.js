be7561d7f0c0c98f804c022c43bef5181bd1e8a5
dimonco <dumitru.cotet@fau.de>
2018-07-03 22:46:19 +0200

    Add camera functionality, but the file sending does not work yet

--- user-app/src/components/MediaSupport/camera.js
+++ user-app/src/components/MediaSupport/camera.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import styles from '../Login/Design';
-import Camera from 'react-native-camera';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
+// import styles from '../Login/Design';
+import { RNCamera } from 'react-native-camera';
 import uploadFile from '../Chat/files';
 import ticket from '../Chat/sendMessages';
 
@@ -14,62 +21,65 @@ export default class CameraAccess extends Component {
     this.state = {
       path: null,
     };
-  }  
-    
-render() {
-return ( 
-        <View style={styles.container}>
-        <Camera 
-        ref={(cam) => {
-          this.camera = cam;
-        }}
-                     style={styles.view}
-                     aspect={Camera.constants.Aspect.fill}
-                     //captureTarget={Camera.constants.CaptureTarget.disk}
-                     //permissionDialogTitle={'Permission to use camera'}
-                     //permissionDialogMessage={'We need your permission to use your camera phone'}
-                     >
-        <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
-        <Text style={styles.capture}
-        // onPress={this.sendPicture.bind(this)}
-        >Send</Text>
-       </Camera>
-         </View>
-       
-   );
- }
+  }
 
-  //takePicture() {
- // const options = {};
-// this.camera.capture({metadata:options}).then((data) => 
-//console.log(data))
-//.catch(err => console.error(err));  
-//}
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style = {styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.auto}
+            permissionDialogTitle={'Permission to use camera'}
+            permissionDialogMessage={'We need your permission to use your camera phone'}
+        />
+        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
+        <TouchableOpacity
+            onPress={this.takePicture.bind(this)}
+            style = {styles.capture}
+        >
+            <Text style={{fontSize: 14}}> SNAP </Text>
+        </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 
-//takePicture() {
-//  if (this.camera) {
- //   const options = { quality: 0.5 };
-  //  const data = this.camera.capture(options)
-  //  console.log(data.uri);
-//  }
-//};
-
-takePicture = async function(camera) {
-  alert('data');
- var options = { quality: 0.5 };
- var data = await camera.takePictureAsync(options);
-  this.setState({ path: data.uri });
-    // this.props.updateImage(data.uri);
-    // console.log('Path to image: ' + data.uri);
-    console.log(data.uri);
-    
-};
-
-
-//sendPicture() {
-//uploadFile(uri, ticket)
-//const { navigate } = this.props.navigation;
-//		navigate("Seventh", { name: "GetMessages" });
-//}
-
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options)      
+      console.log(data.uri);
+      uploadFile(data.uri, ticket).catch(function(error) {
+        console.log('There has been a problem with your fetch operation: ' + error.message);
+         // ADD THIS THROW error
+          throw error;
+        });;
+    }
+  };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  }
+});
