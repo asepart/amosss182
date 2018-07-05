e85ad2040ef6e66f381c84a61ac3521807a809b0
Mark Rudtke <mark.rudtke@gmx.de>
2018-07-05 11:50:05 +0200

    fixed bug
    where app crashed after leaving a project and then joining a
    project that does not exist

--- user-app/src/components/Projects/keyValid.js
+++ user-app/src/components/Projects/keyValid.js
@@ -1,7 +1,7 @@
 import {URL} from '../Login/const';
 import {getAuth,username,psw} from '../Login/auth';
 
-export var key = "";
+export var key = '';
 var val=false;
 
 window.btoa = require('Base64').btoa;
@@ -17,41 +17,34 @@ var response = await fetch(URL + '/join', {
         method: 'POST',
         headers: getAuth(),
         body:  key
-    })
-  
+    }).catch( (error) => {
+    	console.error("Error:" + response.status);
+    	console.error(response.text);}
+    );
 	switch (response.status) {
-		    case 200:
-			      val=true;
-            return true;
+		case 200:
+			val=true;
         case 204:
-			      val=true;
-            return true;
+        	val=true;
         // username or password is invalid    
         case 401:
             val=false;
-            return false;
         // user had already joined 
         case 400:
             val=true;
-            return true;
         // user is not part of the project    
         case 403:
             val=false;
-            return false;
         // key is invalid    
         case 404:
             val=false;
-            return false;
-		default:
-			console.error("Error:" + response.status);
-			console.error(response.text);
+		default:			
 	}
+	return val;
 }
 
 export async function isValid() {
-    if (val)
-        return true;
-	if (key === '')
+    if (key === '')
 		return false;
 	return await validateKey();
 }
