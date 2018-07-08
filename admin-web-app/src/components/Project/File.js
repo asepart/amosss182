e3105efc62c47ef9f26bdc086659b69b7be943ac
8sd <github@sebdu.de>
2018-07-08 21:55:30 +0200

    User ChatImage and Video for media preview

--- admin-web-app/src/components/Project/File.js
+++ admin-web-app/src/components/Project/File.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { Text, View, Button } from 'react-native';
-import {getAuth} from '../shared/auth';
-import {URL} from '../shared/const';
-import {setUpdateBoolean} from '../shared/GlobalState';
+import { getAuth } from '../shared/auth';
+import { URL } from '../shared/const';
+import { setUpdateBoolean } from '../shared/GlobalState';
 import { Link } from 'react-router-dom';
+import { isImage, isVideo } from '../Chat/ChatMessage';
+import { ChatImage } from '../Chat/ChatImage';
+import { ChatVideo } from '../Chat/ChatVideo';
+import CPopup from '../shared/CPopup';
 
 export class File extends Component {
 	constructor(props) {
@@ -19,6 +23,24 @@ export class File extends Component {
 		.catch((error) => {console.error(error);});
 	}
 
+	getPreview () {
+		if (isImage(this.props.name.originalName)){
+			return(
+				<View>
+					<ChatImage src={URL + "/files/" + this.props.name.attachmentId} />
+				</View>
+			);
+		} else if (isVideo(this.props.name.originalName)) {
+			return (
+				<View>
+					<ChatVideo src={URL + "/files/" + this.props.name.attachmentId} />
+				</View>
+			);
+		} else {
+			return (<View></View>);
+		}
+	}
+
 	render() {
 		return (
 			<View style={{flexDireaction:'row'}}>
@@ -28,7 +50,7 @@ export class File extends Component {
 					</Link>
 					{this.props.del === 'true' ?
 						<img src={require('../images/delete.png')} onClick={this.deleteFile.bind(this)} style={{height: 15, width: 15, marginBottom: -5}} alt="delete"/>
-					:<View></View>}
+					: <p>{this.getPreview()}</p>}
 				</Text>
 			</View>
 		);
