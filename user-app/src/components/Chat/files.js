d307bc8e8d4ed90be40ddc1a0c622945f31e830c
Long Do <33006592+longpdo@users.noreply.github.com>
2018-07-09 15:59:48 +0200

    added proper filename extensions for ios files [skip ci]

--- user-app/src/components/Chat/files.js
+++ user-app/src/components/Chat/files.js
@@ -4,33 +4,33 @@ import {getAuth, getAuthForMediaPost} from '../Login/auth';
 import {setMsg, sendMessage} from './sendMessages';
 import moment from 'moment';
 
-var link = '';
-
 export var type = '';
 
 export function setType(asset) {
 	type = asset;
 }
 
-export async function uploadFile (uri, ticket) {
-
+export async function uploadFile (file, ticket) {
+	var filename = '';
 	var ext = '';
-	if(type == 'Photos') {
-		ext = '.jpg';
-	}
-	if(type == 'Videos') {
-		ext = '.mp4';
-		if (Platform.OS === 'ios') {
-			ext = '.MOV';
-		}
-	}
 
-	var filename = (btoa(moment()) + ext);
+	if (Platform.OS === 'ios') {
+		filename = file.filename;
+	}
+	else {
+		if(type == 'Photos') {
+			ext = '.jpg';
+		}
+		if(type == 'Videos') {
+			ext = '.mp4';
+		}
+		filename = (btoa(moment()) + ext);
+	}
 
 	//convert file into FormData
 	const image = {
-					uri: uri,
-					type: 'multipart/form-data',
+					uri: file.uri,
+					//type: 'multipart/form-data',
 					name: filename
 	}
 	const imgBody = new FormData();
@@ -46,21 +46,24 @@ export async function uploadFile (uri, ticket) {
 		return response.text()
 	})
 	.then((responseText) => {
-		console.log(URL + '/files/' + ticket)
+		//TODO: why does this not work for mp4?
 		console.log("promise is: " + responseText)
+		
 		//send file url to chat
-
 		fetch(URL + '/messages/' + ticket + '?attachment=' + responseText, {
 					method: 'POST',
 					headers: getAuth(),
 					body: URL + '/files/' + responseText
 		})
+		.catch((error) => {
+			console.log(error);
+			alert("Sending attachment failed.");
+		});
 	})
 	.catch((error) => {
 		console.log(error);
 		alert("File upload failed.");
 	});
 
-	console.log(ticket);
-	console.log(imgBody);
+	console.log('FormData is: ' + JSON.stringify(imgBody));
 }
